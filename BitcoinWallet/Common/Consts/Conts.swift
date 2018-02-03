f260a4e59823462b9fc1153b0644a29fc77604b9
Romilson Nunes <souzainf3@yahoo.com.br>
2018-02-03 18:49:00 -0200

    [ADD] Base project folders

---
+++ BitcoinWallet/Common/Consts/Conts.swift
@@ -0,0 +1,35 @@
+//
+//  Conts.swift
+//  BitcoinWallet
+//
+//  Created by Romilson Nunes on 05/02/17.
+//  Copyright © 2017 Romilson Nunes. All rights reserved.
+//
+
+import UIKit
+
+let myAppDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+
+struct App {
+    
+    /// Versão vigente da aplicação.
+    static let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+    static let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as? String ?? ""
+
+    static let name = Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? ""
+    
+    
+    // MARK: - Keys
+
+    struct Keys {
+    }
+    
+    
+    // MARK: - Colors
+    
+    struct Color {
+    }
+
+}
+
+
