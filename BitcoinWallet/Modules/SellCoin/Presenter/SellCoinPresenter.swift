7cfbf65ef391e678789a0b27201d184135230ddd
Romilson Nunes <souzainf3@yahoo.com.br>
2018-02-14 13:28:33 -0200

    [ADD] Create sell presenter and interactor

---
+++ BitcoinWallet/Modules/SellCoin/Presenter/SellCoinPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  SellCoinPresenter.swift
+//  BitcoinWallet
+//
+//  Created by Romilson Nunes on 14/02/18.
+//  Copyright © 2018 Romilson Nunes. All rights reserved.
+//
+
+import Foundation
